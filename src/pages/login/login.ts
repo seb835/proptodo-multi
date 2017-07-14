32d46b918454c212296ef7eabec2fb12d59a3998
Seb Pedley <seb@propeller-studios.co.uk>
2017-07-14 15:41:31 +0100

    Added loading dialogs and login/registration error alerts

--- src/pages/login/login.ts
+++ src/pages/login/login.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
 import { Http, Headers } from '@angular/http';
 import { NavController } from 'ionic-angular';
+import { LoadingController } from 'ionic-angular';
+import { AlertController } from 'ionic-angular';
 import { SignupPage } from '../signup/signup';
 import { HomePage } from '../home/home';
 import { ItemsProvider } from '../../providers/items/items';
@@ -14,7 +16,13 @@ export class LoginPage {
   username: string;
   password: string;
 
-  constructor(public nav: NavController, public http: Http, public itemService: ItemsProvider) {
+  constructor(
+    public nav: NavController,
+    public http: Http,
+    public itemService: ItemsProvider,
+    public loadingCtrl: LoadingController,
+    public alertCtrl: AlertController
+  ) {
 
   }
 
@@ -27,11 +35,26 @@ export class LoginPage {
       password: this.password
     };
 
+    let loader = this.loadingCtrl.create({
+      content: "Please wait...",
+      dismissOnPageChange: true
+    });
+    loader.present();
+
     this.http.post('http://10.0.0.205:3000/auth/login', JSON.stringify(credentials), {headers: headers})
       .subscribe(res => {
         this.itemService.init(res.json());
         this.nav.setRoot(HomePage);
       }, (err) => {
+        loader.dismiss();
+
+        let alert = this.alertCtrl.create({
+          title: 'Login Error',
+          subTitle: JSON.parse(err._body).message,
+          buttons: ['Dismiss']
+        });
+        alert.present();
+
         console.log(err);
       });
   }
--- src/pages/signup/signup.ts
+++ src/pages/signup/signup.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
 import { Http, Headers } from '@angular/http';
 import { NavController } from 'ionic-angular';
+import { LoadingController } from 'ionic-angular';
+import { AlertController } from 'ionic-angular';
 import { HomePage } from '../home/home';
 import { ItemsProvider } from '../../providers/items/items';
 
@@ -16,7 +18,13 @@ export class SignupPage {
   password: string;
   confirmPassword: string;
 
-  constructor(public nav: NavController, public http: Http, public itemService: ItemsProvider) {
+  constructor(
+    public nav: NavController,
+    public http: Http,
+    public itemService: ItemsProvider,
+    public loadingCtrl: LoadingController,
+    public alertCtrl: AlertController
+  ) {
 
   }
 
@@ -32,11 +40,38 @@ export class SignupPage {
       confirmPassword: this.confirmPassword
     };
 
+    let loader = this.loadingCtrl.create({
+      content: "Please wait...",
+      dismissOnPageChange: true
+    });
+    loader.present();
+
     this.http.post('http://10.0.0.205:3000/auth/register', JSON.stringify(user), {headers: headers})
       .subscribe(res => {
         this.itemService.init(res.json());
         this.nav.setRoot(HomePage);
       }, (err) => {
+        loader.dismiss();
+
+        // Parse the error/validation message
+        let err_obj = JSON.parse(err._body);
+        let err_msg = err_obj['error'];
+
+        if (err_obj['validationErrors']) {
+          err_msg += "<br /><ul>";
+          for (let key in err_obj['validationErrors']) {
+            err_msg += "<li>"+err_obj['validationErrors'][key][0]+"</li>";
+          }
+          err_msg += "</ul>";
+        }
+
+        let alert = this.alertCtrl.create({
+          title: 'Registration Error',
+          subTitle: err_msg,
+          buttons: ['Dismiss']
+        });
+        alert.present();
+
         console.log(err);
       });
   }
